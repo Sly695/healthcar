514b9c9cbe46ac6fea305065f999d527d5acff08
S7Y <sandaraly1@gmail.com>
2021-05-19 09:16:54 +0200

    dashboard.pt1

--- front/src/App.js
+++ front/src/App.js
@@ -3,31 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Button, DatePicker } from "antd";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
-import SignIn from "./screens/SignIn";
-import BoardSoignant from "./screens/BoardSoignant";
-import BoardAmbulance from "./screens/BoardAmbulance";
-import profilEditTransport from "./screens/profilEditTransport";
-import profilEditSoignant from "./screens/profilEditSoignant";
-import ListeTransport from "./screens/ListeTransport";
+import dashboard from './dashboard'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={SignIn} />
-        <Route exact path="/account-client" component={BoardSoignant} />
-        <Route
-          exact
-          path="/account-edit-client"
-          component={profilEditSoignant}
-        />
-        <Route exact path="/account-ambulance" component={BoardAmbulance} />
-        <Route
-          exact
-          path="/account-edit-ambulance"
-          component={profilEditTransport}
-        />
-        <Route exact path="/list" component={ListeTransport} />
+        <Route exact path="/dashboard" component={dashboard} />
       </Switch>
     </Router>
   );
