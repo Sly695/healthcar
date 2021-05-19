720eea59e3c361c40623dfee2e40757309b920b1
Armand <armand.gillot@gmail.com>
2021-05-19 10:22:12 +0200

    dashboard sandara

--- front/src/App.js
+++ front/src/App.js
@@ -3,23 +3,18 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
-
-import dashboard from './dashboard'
+import dashboard from "./dashboard";
 import SignIn from "./screens/SignIn";
 // import BoardSoignant from "./screens/BoardSoignant";
 // import BoardAmbulance from "./screens/BoardAmbulance";
 import Profil from "./component/ScreenProfil";
 // import profilEditSoignant from "./screens/profilEditSoignant";
 // import ListeTransport from "./screens/ListeTransport";
-import dashboard from "./screens/dashboard";
 
 function App() {
   return (
     <Router>
       <Switch>
-
-        <Route exact path="/dashboard" component={dashboard} />
-
         <Route exact path="/" component={SignIn} />
         <Route exact path="/account-edit-client" component={Profil} />
         <Route exact path="/dashboard" component={dashboard} />
