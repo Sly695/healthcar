138dd0b0ff24c8a84f4696d017cc5e1491c46c33
Olivier PIERRE <olivier@opierre.fr>
2021-05-19 17:20:27 +0200

    page dashboard partie 2

--- front/src/component/Nav.js
+++ front/src/component/Nav.js
@@ -1,96 +1,64 @@
 import React, { useState } from "react";
 
 import {
-  Avatar,
-  List,
   Card,
   Layout,
-  Row,
-  Col,
   Image,
   Menu,
-  Button,
-  DatePicker,
+  Space,
 } from "antd";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 import {
-  CarOutlined,
-  CheckOutlined,
-  DashboardOutlined,
   UserOutlined,
-  PieChartOutlined,
-  LogoutOutlined,
-  FileOutlined,
-  TeamOutlined,
-  DesktopOutlined,
-  FieldTimeOutlined,
-  ReloadOutlined,
   CheckCircleTwoTone,
-  BellOutlined,
-  CloseCircleOutlined,
 } from "@ant-design/icons";
 
 // import logobleu from "../../public/images/Logobleu.svg";
 
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useRouteMatch,
-} from "react-router-dom";
+export default function Nav() {
 
-function Nav() {
   const { Meta } = Card;
-
   const { SubMenu } = Menu;
+  const { Sider } = Layout;
 
-  const { Header, Footer, Sider, Content } = Layout;
   return (
-    <Col xs={4} sm={4} md={4} lg={4} xl={4}>
-      <Menu
-        style={{
-          width: "100%",
-          height: "100vh",
-          justifyContent: "space-between",
-        }}
-      >
-        <img
-          style={{ width: "100%", padding: "10px" }}
-          // src={logobleu}
-          alt="React Logo"
-        />
-        <SubMenu
-          style={{ borderBottom: "1px solid #F0F2F5", color: "orange" }}
-          key="sub0"
-          title="Username"
-        ></SubMenu>
-        <SubMenu
-          style={{ height: "15%" }}
-          key="sub1"
-          icon={<DashboardOutlined />}
-          title="Tableau de bord"
-        ></SubMenu>
-        <SubMenu
-          style={{ height: "15%" }}
-          key="sub2"
-          icon={<CarOutlined />}
-          title="Transports"
-        ></SubMenu>
-        <SubMenu
-          style={{ height: "15%", borderBottom: "1px solid #F0F2F5" }}
-          key="sub3"
-          icon={<UserOutlined />}
-          title="Gestion du profil"
-        ></SubMenu>
-        <SubMenu
-          key="sub4"
-          icon={<LogoutOutlined style={{ color: "orange" }} />}
-          title="Deconnexion"
-        ></SubMenu>
-      </Menu>
-    </Col>
-  );
-}
 
-export default Nav;
+    <Sider 
+    theme="light"
+    breakpoint="lg"
+    collapsedWidth="0"
+    onBreakpoint={broken => {
+      console.log(broken);
+    }}
+    onCollapse={(collapsed, type) => {
+      console.log(collapsed, type);
+    }}
+    style={{
+      width: "100%",
+      height: "100vh",
+      justifyContent: "space-between",
+    }}>
+
+      <Image 
+      className="logo" 
+      src='../../public/images/logo.svg'
+      preview='false'
+      />
+
+        <Menu  mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<CheckCircleTwoTone />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<CheckCircleTwoTone />}>
+              nav 3
+            </Menu.Item>
+          </Menu>          
+
+      </Sider>
+
+  )
+}
