6a31c7bf816adebc7413db968d1e68873cea73a6
Armand <armand.gillot@gmail.com>
2021-05-21 11:40:45 +0200

    bug heroku

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, "/front/build")));
+app.use(express.static(path.join(__dirname, "front/build")));
+
+app.get("*", function (_, res) {
+  res.sendFile(path.join(__dirname, "front/build/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
